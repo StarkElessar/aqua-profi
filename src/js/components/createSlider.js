5cd8faea94981e259bf9d320a2114955ca56b06c
Stark <serj.elessar@gmail.com>
2023-06-20 15:16:08 +0300

    fix: поправил пути для изображений

--- src/js/components/createSlider.js
+++ src/js/components/createSlider.js
@@ -1,38 +1,41 @@
-import { createElement } from '../helpers/elementFactories.js';
-const createSlide = (imageSrc, isFancyBox) => {
-	const commonAttributes = {
-		class: 'product-slider__image-ibg',
-	};
+import { createElement } from "../helpers/elementFactories.js";
 
-	const fancyBoxAttributes = {
-		class: 'main__image-ibg_contain',
-		'data-fancybox': 'gallery',
-		'data-src': `images/product/image${imageSrc}`,
-	};
+const createSlide = (imageSrc, isFancyBox, category, id) => {
+  const commonAttributes = {
+    class: "product-slider__image-ibg",
+  };
 
-	const slideContent = createElement({
-		typeElement: 'div',
-		attributes: isFancyBox ? fancyBoxAttributes : commonAttributes,
-		innerHTML: `
-			<img src="images/product/image${imageSrc}" alt="">
+  const fancyBoxAttributes = {
+    class: "main__image-ibg_contain",
+    "data-fancybox": "gallery",
+    "data-src": `images/products/${category}/${id}/${imageSrc}`,
+  };
+
+  const slideContent = createElement({
+    typeElement: "div",
+    attributes: isFancyBox ? fancyBoxAttributes : commonAttributes,
+    innerHTML: `
+			<img src='images/products/${category}/${id}/${imageSrc}' alt=''>
 		`,
-	});
+  });
 
-	return createElement({
-		typeElement: 'div',
-		attributes: { class: 'swiper-slide' },
-		children: [slideContent],
-	});
+  return createElement({
+    typeElement: "div",
+    attributes: { class: "swiper-slide" },
+    children: [slideContent],
+  });
 };
 
-const createSlider = (images, isFancyBox) => {
-	const slides = images.map((img) => createSlide(img, isFancyBox));
+const createSlider = (images, isFancyBox, category, id) => {
+  const slides = images.map((img) =>
+    createSlide(img, isFancyBox, category, id)
+  );
 
-	return createElement({
-		typeElement: 'div',
-		attributes: { class: 'swiper-wrapper' },
-		children: slides,
-	});
+  return createElement({
+    typeElement: "div",
+    attributes: { class: "swiper-wrapper" },
+    children: slides,
+  });
 };
 
 export default createSlider;
